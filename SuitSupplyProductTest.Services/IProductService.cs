75831ff51e42d51d85d4f7484adc59a71d6a164d
Sefa Ümit Oray <sefa@fastmail.com>
2018-06-03 17:53:40 +0300

    Business layer added for product

---
+++ SuitSupplyProductTest.Services/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SuitSupplyProductTest.Services
+{
+    public interface IProductService
+    {
+        Product AddProduct(Product product);
+        Product GetProductByCode(string code);
+        Product GetProductById(int id);
+        IEnumerable<Product> GetProducts();
+        void UpdateProduct(Product product);
+        void DeleteProduct(Product product);
+        void DeleteProduct(int productId);
+    }
+}
