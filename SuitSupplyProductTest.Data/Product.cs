0c302ca221daef48a1400312a68b6038a45ccdea
Sefa Ümit Oray <sefa@fastmail.com>
2018-06-03 14:03:52 +0300

    added product entity

---
+++ SuitSupplyProductTest.Data/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SuitSupplyProductTest.Data
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Code { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public string Photo { get; set; }
+
+        [Range(0, 999, ErrorMessage = "Price must be between 0 and 999")]
+        public decimal Price { get; set; }
+
+        public DateTime LastUpdated { get; set; }
+    }
+}
