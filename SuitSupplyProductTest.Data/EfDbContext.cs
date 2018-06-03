657cc94704153842acba57f7514be15aa06a6985
Sefa Ümit Oray <sefa@fastmail.com>
2018-06-03 14:30:02 +0300

    ef context added

---
+++ SuitSupplyProductTest.Data/EfDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuitSupplyProductTest.Data
+{
+    public class EfDbContext : DbContext
+    {
+        private readonly string _connectionStr;
+
+        public EfDbContext(string connectionStr)
+        {
+            _connectionStr = connectionStr;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionStr);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Product>()
+                .ToTable("Products");
+        }
+
+        internal DbSet<Product> Products { get; set; }
+    }
+}
