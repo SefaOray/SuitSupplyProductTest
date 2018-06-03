70d022d46d6132667523729ed3542619abea330d
Sefa Ümit Oray <sefa@fastmail.com>
2018-06-03 14:06:37 +0300

    IProductDataAccess methods defined

--- SuitSupplyProductTest.Data/IProductDataAccess.cs
+++ SuitSupplyProductTest.Data/IProductDataAccess.cs
@@ -1,8 +1,14 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
 namespace SuitSupplyProductTest.Data
 {
     public interface IProductDataAccess
     {
+        Product InsertProduct(Product product);
+        Product GetProductById(int id);
+        Product GetProductByCode(string code);
+        IEnumerable<Product> GetProducts();
+        void UpdateProduct(Product product);
+        void DeleteProduct(Product product);
     }
 }
