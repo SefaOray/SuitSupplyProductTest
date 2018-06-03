e12b6cee795da2cbac639837a5acc76a6fb1bb1a
Sefa Ümit Oray <sefa@fastmail.com>
2018-06-03 14:11:56 +0300

    data layer for product access

---
+++ SuitSupplyProductTest.Data/ProductDataAccess.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuitSupplyProductTest.Data
+{
+    public class ProductDataAccess : IProductDataAccess
+    {
+        public void DeleteProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetProductByCode(string code)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetProductById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product InsertProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
