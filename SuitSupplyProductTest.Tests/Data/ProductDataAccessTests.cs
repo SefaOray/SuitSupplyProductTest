982890080032bcf03f9107f5d85d9c1d2168504d
Sefa Ümit Oray <sefa@fastmail.com>
2018-06-03 14:16:09 +0300

    added red tests for product data access

--- SuitSupplyProductTest.Tests/Data/ProductDataAccessTests.cs
+++ SuitSupplyProductTest.Tests/Data/ProductDataAccessTests.cs
@@ -9,25 +9,32 @@ namespace SuitSupplyProductTest.Tests
         private readonly IProductDataAccess _productDataAccess;
         public ProductDataAccessTests()
         {
-            
+            _productDataAccess = new ProductDataAccess();
         }
+
         [TestMethod]
         public void InsertProductShouldReturnInsertedProduct()
         {
-            var product = new Product();
+            var product = new Product() { Code = "Code", Name = "Name", Price = 123 };
+            var insertedProduct = _productDataAccess.InsertProduct(product);
 
+            Assert.AreEqual("Code", insertedProduct.Code);
+            Assert.AreEqual("Name", insertedProduct.Name);
+            Assert.AreEqual(123, insertedProduct.Price);
         }
 
         [TestMethod]
         public void GetProductByIdShouldReturnProductWithSameId()
         {
-            var product = new Product();
+            var product = _productDataAccess.GetProductById(1);
+            Assert.AreEqual(1, product.Id);
         }
 
         [TestMethod]
         public void GetProductByCodeShouldReturnProductWithSameCode()
         {
-            var product = new Product();
+            var product = _productDataAccess.GetProductByCode("code");
+            Assert.AreEqual("code", product.Code);
         }
     }
 }
